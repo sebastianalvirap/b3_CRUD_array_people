82658e67497102a8cb671e0b7521adeb453883c4
sebastianalvirap <juanoalvira97@gmail.com>
2024-10-02 21:10:05 -0500

    Form People

--- src/components/people.jsx
+++ src/components/people.jsx
@@ -1,7 +1,39 @@
 import PropTypes from "prop-types";
 import { Person } from "./Person";
+import { useState } from "react";
 
 export const People = ( { people, setPeople } ) => {
+
+  //estado para gestionar elId de la persona que se esta editando
+  const [editingId, setEditingId] = useState(null);
+
+  //estado para almacenar temporalmente los datos de la persona que se esta editando
+  const [editedPerson, setEditedPerson] =useState({
+    name: '',
+    role:'',
+    img: '',
+  })
+
+  //metodo para manejar o gestionar los campos del formulario
+  const handleChange =(e) => {
+    const { name, value } = e.target;
+    setEditedPerson(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  //Método para crear una nueva persona en el Team
+  const handleCreate = (e) => { 
+    e.preventDefault(); //evita que se pierdan cambios cuando se refresque la pagina
+
+    //agregar une persona al array
+    setPeople([...people, {id: people.length +1, ...editedPerson }]) //...people deja people como esta, sin esto se borra todo
+
+    //Reiniciar el estado del formulario
+    setEditedPerson({name:'', role:'',img:''});
+  };
+
   return (
     <div>
       <h2 className='text-center my-4'>IT Team</h2>
@@ -22,6 +54,17 @@ export const People = ( { people, setPeople } ) => {
             })
           }
         </div>
+      </div>
+      {/* formulario */}
+      <div className="container ">
+        <h2 className="text-center mt-4">Crear nuevo empleado</h2>
+        <form action="">
+          <div>
+            <label htmlFor="name">Nombre</label>
+            <input type="text" name="name" value={editedPerson.name} required className="form-control"/>
+          </div>
+        </form>
+
       </div>
     </div>
   )
